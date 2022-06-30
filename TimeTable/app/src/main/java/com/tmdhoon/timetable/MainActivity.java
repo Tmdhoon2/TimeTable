0f853313814084cf03cd14880b3b737f5ac1fef9
Tmdhoon2 <hks026naver@gmail.com>
2022-06-30 22:25:31 +0900

    Editting

--- TimeTable/app/src/main/java/com/tmdhoon/timetable/MainActivity.java
+++ TimeTable/app/src/main/java/com/tmdhoon/timetable/MainActivity.java
@@ -1,33 +1,52 @@
 package com.tmdhoon.timetable;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.tmdhoon.timetable.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
     private FirebaseDatabase database;
     private DatabaseReference databaseReference;
-    private ActivityMainBinding binding;
 
-    @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        database =FirebaseDatabase.getInstance();
+        databaseReference.child("Table").child("Monday").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
-        databaseReference = database.getReference("Table");
 
-        Table table = 
 
-        binding.monday01.setText();
+            }
 
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        })
     }
 }
--- TimeTable/app/src/main/java/com/tmdhoon/timetable/Table.java
+++ TimeTable/app/src/main/java/com/tmdhoon/timetable/Table.java
@@ -1,15 +1,40 @@
 package com.tmdhoon.timetable;
 
 public class Table {
-    private String table;
+    private String monday01;
+    private String monday02;
+    private String monday03;
 
-    public Table(){}
-
-    public String getTable() {
-        return table;
+    public Table(){
     }
 
-    public void setTable(String table) {
-        this.table = table;
+    public String getMonday01() {
+        return monday01;
+    }
+
+    public void setMonday01(String monday01) {
+        this.monday01 = monday01;
+    }
+
+    public String getMonday02() {
+        return monday02;
+    }
+
+    public void setMonday02(String monday02) {
+        this.monday02 = monday02;
+    }
+
+    public String getMonday03() {
+        return monday03;
+    }
+
+    public void setMonday03(String monday03) {
+        this.monday03 = monday03;
+    }
+
+    public Table(String monday01, String monday02, String monday03) {
+        this.monday01 = monday01;
+        this.monday02 = monday02;
+        this.monday03 = monday03;
     }
 }
